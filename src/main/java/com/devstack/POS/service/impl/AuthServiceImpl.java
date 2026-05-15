27ae39bb0ad44733c041c68eb3febdde8510215e
Naveen Rajapaksha <navee.rajapaksha83@gmail.com>
2026-05-15 21:51:49 +0530

    8

--- src/main/java/com/devstack/POS/service/impl/AuthServiceImpl.java
+++ src/main/java/com/devstack/POS/service/impl/AuthServiceImpl.java
@@ -3,11 +3,31 @@ package com.devstack.POS.service.impl;
 import com.devstack.POS.dto.request.LoginRequestDTO;
 import com.devstack.POS.dto.request.RegisterRequestDTO;
 import com.devstack.POS.dto.response.AuthResponseDTO;
+import com.devstack.POS.entity.ROLE_TYPES;
+import com.devstack.POS.entity.SystemUser;
+import com.devstack.POS.exception.DuplicateEntryException;
 import com.devstack.POS.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     @Override
     public void register(RegisterRequestDTO dto) {
+        if (systemUserRepo.existsByEmail(dto.getEmail())){
+            throw new DuplicateEntryException("Email is Already exists");
+        }
+
+        SystemUser user = SystemUser.builder()
+                .fullName(dto.getFullName())
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .role(ROLE_TYPES.USER)
+                .isActive(true)
+                .build();
+
+        systemUserRepo.save(user);
 
     }
 
