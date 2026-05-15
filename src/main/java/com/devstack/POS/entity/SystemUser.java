0c3dd3c176f92fe14fb4367d315151c63587cf44
Naveen Rajapaksha <navee.rajapaksha83@gmail.com>
2026-05-15 10:06:34 +0530

    3

--- src/main/java/com/devstack/POS/entity/SystemUser.java
+++ src/main/java/com/devstack/POS/entity/SystemUser.java
@@ -1,4 +1,32 @@
 package com.devstack.POS.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "system_user")
+@Builder
 public class SystemUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false, length = 100)
+    private String fullName;
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ROLE_TYPES role;
+    @Column(nullable = false)
+    private boolean isActive = true;
+
 }
